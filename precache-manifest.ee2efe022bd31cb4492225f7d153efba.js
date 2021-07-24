f9c9217b1572a89f434156cf6caa96046db878f4
John <jozaman96@gmail.com>
2021-07-24 18:00:56 +0300

    Updates

---
+++ precache-manifest.ee2efe022bd31cb4492225f7d153efba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff288b356a774f50f9f4d6db1bcfef0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b1a4e95474e07599171",
+    "url": "./static/css/main.8bed03de.chunk.css"
+  },
+  {
+    "revision": "f4a6e332316f4a7ffe8f",
+    "url": "./static/js/2.50763e96.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.50763e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b1a4e95474e07599171",
+    "url": "./static/js/main.dbd6c3bd.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
